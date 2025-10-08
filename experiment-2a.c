d427a87effb622ae64bc9482b1736401c687383c
karuneshj100108-coder <karuneshj100108@eng.rizvi.edu.in>
2025-10-08 20:40:39 +0530

    Update and rename experiment-1c.c to experiment-2a.c

---
+++ experiment-2a.c
@@ -0,0 +1,18 @@
+/* Name: Karunesh Jadhav
+ UIN: 251C019
+Division:Civil
+Div: B*/
+#include <stdio.h>
+int main()
+{
+int whether;
+printf("enter a number: ");
+scanf("%d", &num);
+if (num %2==0){
+printf("%d is an even number.\n", num);
+}else {
+printf ("%d is an odd number.\n", num);
+}
+  return 0:
+
+}
