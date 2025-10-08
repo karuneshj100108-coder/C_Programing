818f01766f03e2dd1150f8ab7ceaeeca381efc52
karuneshj100108-coder <karuneshj100108@eng.rizvi.edu.in>
2025-10-08 20:53:19 +0530

    Create experiment2b.c

---
+++ experiment2b.c
@@ -0,0 +1,22 @@
+/* Name: karunesh Jadhav
+Age :18
+Class:civil
+Division: B
+UIN : 251C019*/
+#include <stdio.h>
+int main() 
+{
+  int num1;
+  int num2;
+  int sum=0;
+  printf("enter a first number ");
+  scanf("%d", &num1);
+  scanf("%d", &num2);
+  for(int i = num1 ;i<=num2:i++){
+  if (i%2! =0){
+  sum +=1;
+  printf ("sum is %d", sum);
+  }
+  }
+  return 0;
+}
