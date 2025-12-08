235a748aae8c025cebe373724859db6b84fcd07c
karuneshj100108-coder <karuneshj100108@eng.rizvi.edu.in>
2025-12-08 19:43:30 +0530

    Update experiment-2a.c

--- experiment-2a.c
+++ experiment-2a.c
@@ -1,18 +1,20 @@
 /* Name: Karunesh Jadhav
- UIN: 251C019
+Age :18
+UIN: 251C019
 Division:Civil
 Div: B*/
-#include <stdio.h>
-int main()
-{
-int whether;
-printf("enter a number: ");
-scanf("%d", &num);
-if (num %2==0){
-printf("%d is an even number.\n", num);
-}else {
-printf ("%d is an odd number.\n", num);
-}
-  return 0:
 
+#include <stdio.h>
+int main(){
+    int num;
+
+    printf("enter a number: ");
+    scanf("%d", &num);
+
+    if (num %2==0)
+    printf("%d is an even number.\n", num);
+    else 
+    printf ("%d is an odd number.\n", num);
+
+return 0;
 }
